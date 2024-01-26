1599e47c9b19668cd4d4a8d087f27278a86274fe
Hannah Ellis <hannah.ellis@f9gdhyfqxm.home>
2024-01-26 15:19:17 +0000

    pulling out code

--- test/partition.test.ts
+++ test/partition.test.ts
@@ -1,22 +1,4 @@
-type Partition<T> = [T[], T[]];
-const first = <T>(item: T): Partition<T> => [[item], []];
-const second = <T>(item: T): Partition<T> => [[], [item]];
-const lift = <T>(item: T): Partition<T>[] => [first(item), second(item)];
-const flatten = <T>(
-  [a0, a1]: Partition<T>,
-  [b0, b1]: Partition<T>
-): Partition<T> => [a0.concat(b0), a1.concat(b1)];
-
-const partitions = <T>(items: T[]): Partition<T>[] =>
-  items.map(lift).reduce(
-    (acc, [f, s]) => {
-      const firsts = acc.map((x) => flatten(x, f));
-      const seconds = acc.map((x) => flatten(x, s));
-      return firsts.concat(seconds);
-    },
-    [[[], []]]
-  );
-
+import partitions from "../src/partitioning/partitions";
 describe("partition", () => {
   test("empty", () => {
     const results = partitions([]);
@@ -45,16 +27,14 @@ describe("partition", () => {
     const results = partitions([1, 2, 3]);
     expect(results).toHaveLength(8);
     expect(results).toStrictEqual([
-      [
-        [[1, 2, 3], []],
-        [[2, 3], [1]],
-        [[1, 3], [2]],
-        [[3], [1, 2]],
-        [[1, 2], [3]],
-        [[2], [1, 3]],
-        [[1], [2, 3]],
-        [[], [1, 2, 3]],
-      ],
+      [[1, 2, 3], []],
+      [[2, 3], [1]],
+      [[1, 3], [2]],
+      [[3], [1, 2]],
+      [[1, 2], [3]],
+      [[2], [1, 3]],
+      [[1], [2, 3]],
+      [[], [1, 2, 3]],
     ]);
   });
   test("4 element", () => {
